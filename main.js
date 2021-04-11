62279215d32c0b6d48dcbee29bf01602107c5a2d
mayak <ma.y.ak@ya.ru>
2021-04-11 14:24:03 +0300

    Add homework-2

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-const $arenas = document.querySelector('.arenas');
+const $arenas = document.querySelector(".arenas");
+const $randomButton = document.querySelector(".button");
 
 const player1 = {
   player: 1,
-  name: "Subzero",
-  hp: 70,
+  name: "SUBZERO",
+  hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
   weapon: ["kunai"],
   attack: function () {
     console.log(player1.name + " Fight...");
   },
-}
+};
 const player2 = {
   player: 2,
-  name: "Scorpion",
-  hp: 40,
+  name: "SCORPION",
+  hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["katana"],
   attack: function () {
     console.log(player2.name + " Fight...");
   },
-}
+};
 
 function createElement(tag, className) {
   const $tag = document.createElement(tag);
   if (className) {
     $tag.classList.add(className);
-  };
+  }
   return $tag;
 }
 
 function createPlayer(playerObj) {
-  const $player = createElement("div", 'player' + playerObj.player);
+  const $player = createElement("div", "player" + playerObj.player);
   const $progressbar = createElement("div", "progressbar");
   const $character = createElement("div", "character");
   const $life = createElement("div", "life");
@@ -54,3 +55,48 @@ function createPlayer(playerObj) {
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
+
+
+function changeHP(count, player) {
+  const $playerLife = document.querySelector(
+    ".player" + player.player + " .life"
+  );
+  player.hp -= count;
+  $playerLife.style.width = player.hp + "%";
+
+  if (player.hp <= 0) {
+    $playerLife.style.width = '0%';
+    player.hp = 0;
+  }
+
+  
+}
+
+
+function random(num) {
+  return Math.ceil(Math.random() * num);
+}
+
+
+
+$randomButton.addEventListener("click", function () {
+  changeHP(random(20), player1);
+  changeHP(random(20), player2);
+
+  if (player1.hp > 0 && player2.hp==0){
+    $arenas.appendChild(playerWins(player1.name))
+
+}
+if (player2.hp > 0 && player1.hp==0){
+    $arenas.appendChild(playerWins(player2.name))
+
+}
+})
+
+function playerWins(name) {
+  const $winTitle = createElement("div", "winTitle");
+  $winTitle.innerText = name + " wins";
+  $randomButton.disabled = true;
+  return $winTitle;
+}
+
